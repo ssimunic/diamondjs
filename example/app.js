b68072fa08fb2bbd549be1fe7de3d9431964ef5a
silvio <silvio.simunic@gmail.com>
2016-08-29 00:46:13 +0200

    Added basic middleware

--- example/app.js
+++ example/app.js
@@ -1,13 +1,14 @@
 const diamond = require('../lib/diamond');
+const morgan = require('morgan');
 
-diamond.useLogger(true);
+diamond.use(morgan('combined'));
 
-diamond.route('GET', '/', function(req, res) {
+diamond.route('GET', '/', function (req, res) {
   const name = req.query.name;
   res.write(`Hello ${name}`);
 });
 
-diamond.route('POST', '/', function(req, res) {
+diamond.route('POST', '/', function (req, res) {
   res.write('Home post');
 });
 
