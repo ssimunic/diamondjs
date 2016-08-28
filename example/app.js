58bf2b27617d01d7de58808446c98b686620b563
silvio <silvio.simunic@gmail.com>
2016-08-29 01:32:59 +0200

    Middleware tweaked

--- example/app.js
+++ example/app.js
@@ -1,6 +1,17 @@
 const diamond = require('../lib/diamond');
 const morgan = require('morgan');
 
+
+diamond.use(function(req, res, next) {
+  console.log('Middleware 1');
+  next();
+});
+
+diamond.use(function(req, res, next) {
+  console.log('Middleware 2');
+  next();
+});
+
 diamond.use(morgan('combined'));
 
 diamond.route('GET', '/', (req, res) => {
