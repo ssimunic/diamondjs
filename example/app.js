782009f3f9ad4ece77c67b6b43abc4e1398decab
silvio <silvio.simunic@gmail.com>
2016-08-29 01:16:15 +0200

    ES6 changes

--- example/app.js
+++ example/app.js
@@ -3,12 +3,12 @@ const morgan = require('morgan');
 
 diamond.use(morgan('combined'));
 
-diamond.route('GET', '/', function (req, res) {
+diamond.route('GET', '/', (req, res) => {
   const name = req.query.name;
   res.write(`Hello ${name}`);
 });
 
-diamond.route('POST', '/', function (req, res) {
+diamond.route('POST', '/', (req, res) => {
   res.write('Home post');
 });
 
